19c145873513f69a31f743694182e5da2168e51e
Treesha-sg <s139246@student.squ.edu.om>
2026-05-02 23:44:55 +0400

    Update unishop_database.sql

--- unishop_database.sql
+++ unishop_database.sql
@@ -167,3 +167,7 @@ UNION ALL
 SELECT 'wishlist', COUNT(*) FROM wishlist
 UNION ALL
 SELECT 'bills',    COUNT(*) FROM bills;
+UNION ALL
+SELECT 'messages',  COUNT (*) FROM messages;
+UNION ALL
+SELECT 'responses', COUNT (*) FROM responses;
