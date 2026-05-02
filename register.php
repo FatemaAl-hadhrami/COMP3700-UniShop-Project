f7e3beb0d5e9ed74b5498ef24475e68f80e1a98f
FatemaAl-hadhrami <s134146@student.squ.edu.om>
2026-05-02 12:31:19 +0400

    Update register.php

--- register.php
+++ register.php
@@ -80,6 +80,7 @@ class User {
       <a class="nav-link text-white" href="questionnaire.php">Questionnaire</a>
       <a class="nav-link text-white" href="calculator.php">Calculator</a>
       <a class="nav-link text-white" href="funpage.php">Fun Page</a>
+        <a class="nav-link text-white" href="products.php">Products</a>
       <a class="nav-link text-white" href="wish_list.php">Wish List</a>
     </div>
 
